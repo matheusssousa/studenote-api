ed1fe886a3f6661b6ad87aa5dcb90ab7f0caaca7
matheusssousa <matheussousab2014@gmail.com>
2023-08-11 15:48:07 -0300

    feat: remove likes on table notas

--- database/migrations/2023_07_12_167232_create_notas_table.php
+++ database/migrations/2023_07_12_167232_create_notas_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->unsignedBigInteger('disciplina_id');
             $table->unsignedBigInteger('user_id');  
             $table->boolean('annotation_community');
-            $table->integer('likes');
 
             // CHAVE ESTRANGEIRA DE USUÁRIO
             $table->foreign('user_id')->references('id')->on('users');
